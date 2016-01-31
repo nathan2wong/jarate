6c27e58d352fa8302058e7a6c90e153b1a52097a
Ganesh Valliappan <ganeshv757@gmail.com>
2016-01-31 02:18:41 -0800

    500 error solved

--- script.php
+++ script.php
@@ -1,3 +1,7 @@
 <script type="text/javascript" src="js/jquery.js"></script>
 <script type="text/javascript" src="js/materialize.min.js"></script>
 <script type="text/javascript" src="js/typed.js"></script>
+<script type="text/javascript" src="js/wow.min.js"></script>
+<script type="text/javascript">
+    new WOW().init();
+</script>
