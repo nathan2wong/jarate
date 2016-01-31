20c88415d171482668c440388ba5407676ad17f8
Nathan Wong <nathan2wong@gmail.com>
2016-01-31 02:11:01 -0800

    favvicon

--- index.php
+++ index.php
@@ -6,6 +6,9 @@
 <html>
     <head>
         <?php include("head.php"); ?>
+        <link rel="icon" 
+      type="image/ico" 
+      href="favi.ico">
     </head>
     <body>
         <?php include("navbar-reg.php"); ?>
