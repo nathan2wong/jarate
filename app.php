29e6a26f0279293730e14d494c35c7b39706d3c1
Ganesh Valliappan <ganeshv757@gmail.com>
2016-01-31 04:41:27 -0800

    change image on upload

--- app.php
+++ app.php
@@ -21,7 +21,7 @@
             <div class="container" style="margin-top: 1em;">
                 <div class="row center valign-wrapper">
                     <div id="user-image" class="col s12 m9">
-                        <img class="z-depth-3" src="http://placehold.it/400x575">
+                        <img class="z-depth-3" src="#" alt="http://placehold.it/400x575">
                     </div>
                     <div class="col s12 m3">
                         <form action="#">
@@ -29,7 +29,7 @@
                                 <div class="btn">
                                     <i class="material-icons">perm_media</i>
                                     <span>Choose Image!</span>
-                                    <input type="file">
+                                    <input type="file" onChange="readUrl(this);">
                                 </div>
                             </div>
                         </form>
@@ -40,5 +40,22 @@
 
         <?php include("footer.php"); ?>
         <?php include("script.php"); ?>
+
+        <script type="text/javascript">
+            function readURL(input) {
+                if (input.files && input.files[0]) {
+                    var reader = new FileReader();
+
+                    reader.onload = function (e) {
+                        $('#blah')
+                            .attr('src', e.target.result)
+                            .width(150)
+                            .height(200);
+                    };
+
+                    reader.readAsDataURL(input.files[0]);
+                }
+            }
+        </script>
     </body>
 </html>
--- script.php
+++ script.php
@@ -1,7 +1,10 @@
-<script type="text/javascript" src="js/jquery.js"></script>
 <script type="text/javascript" src="js/materialize.min.js"></script>
 <script type="text/javascript" src="js/typed.js"></script>
 <script type="text/javascript" src="js/wow.min.js"></script>
+<script type="text/javascript" src="js/chart.min.js"></script>
+<link class="jsbin" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1/themes/base/jquery-ui.css" rel="stylesheet" type="text/css" />
+<script class="jsbin" src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+<script class="jsbin" type="text/javascript" src="js/jquery.js"></script>
 <script type="text/javascript">
     new WOW().init();
 </script>
