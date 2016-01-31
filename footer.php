77f3062ae74a7bd2cfd9a8ddb6228f7ca5588b22
Nathan Wong <nathan2wong@gmail.com>
2016-01-31 02:33:06 -0800

    footer

--- footer.php
+++ footer.php
@@ -1,9 +1,9 @@
 <footer class="page-footer">
     <div class="container">
-        <p class="white-text center">Have any questions or concerns? Email us at <a class="red-text text-lighten-3" href="mailto:mvnhsofficers@gmail.com">idk@gmail.com</a><br>Website created by Ganesh, Nathan, and Jessica</p>
+        <p class="white-text center">Have any questions or concerns? Email us at <a class="red-text text-lighten-3" href="mailto:hello@jarate.co">hello@jarate.co</a><br>Website created by Ganesh, Nathan, and Jessica</p>
         <div class="footer-copyright">
             <div class="container center">
-                © Sketchy Website, All Rights Reserved
+                ©2016 Jarate Co., All Rights Reserved
             </div>
         </div>
     </div>
