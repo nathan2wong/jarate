94f65859c0ace5f0ceac0eed0af0ceab1d1b97c6
Ganesh Valliappan <ganeshv757@gmail.com>
2016-01-31 02:57:53 -0800

    sign up created

--- signup.php
+++ signup.php
@@ -10,9 +10,10 @@
             $user = stripslashes($_POST['email']);
             $pass = sha1(stripslashes($_POST['pass']));
             $query = $connect->query("SELECT * FROM `users` WHERE `email`='$user' AND `pass` = '$pass' LIMIT 1");
-            $max = $connect->query("SELECT * FROM `users` ORDER BY `ID` DESC")
             if($query->num_rows < 0) {
                 $connect->query("INSERT INTO `users` (`ID`, `fname`, `lname`, `email`, `pass`) VALUES()")
+                $max = $connect->query("SELECT * FROM `users` ORDER BY `ID` DESC LIMIT 1");
+                $max = mysqli_fetch_array($max)
             } else {
                 $msg = "Email already registered!";
             }
