b53ae44777f5b516dbcceacbc3deb8611a4ef7a2
Nathan Wong <nathan2wong@gmail.com>
2016-01-31 12:17:30 -0800

    nav

--- navbar-reg.php
+++ navbar-reg.php
@@ -4,8 +4,8 @@
             <a href="index" class="brand-logo center black-text"><img src="favi.ico" height="40px" width="40px" style="margin-top: 12px;"></a>
             <a href="#" data-activates="mobile-nav" class="button-collapse black-text"><i  class="material-icons">menu</i></a>
             <ul class="left hide-on-med-and-down black-text">
-                <li><a href="app" class="black-text">Try Now!<i class="material-icons">visibility</i></a></li>
-                <li><a class="waves-effect waves-light black-text" href="about">About Us & Technology</a></li>
+                <li><a href="app" class="black-text">Try Now!</a></li>
+                <li><a href="about" class="black-text">About Us & Technology<i class = "material-icons">visibility</i></a></li>
             </ul>
             <ul id="nav" class="right hide-on-med-and-down">
                 <li><a class="waves-effect waves-light black-text" href="signup"><i class="material-icons left">group</i>Register</a></li>
